09de696b97e81f811fa648d19a8587ff214dbd5e
Krot9ira <90332757+Krot9ira@users.noreply.github.com>
2021-12-09 21:38:54 +0300

    Add project to folder

---
+++ Source/TurtlesLife/NestButton.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NestButton.h"
+
+// Sets default values
+ANestButton::ANestButton()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ANestButton::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANestButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TurtlesLife/TurtlesLifeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TurtlesLifeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURTLESLIFE_API ATurtlesLifeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
